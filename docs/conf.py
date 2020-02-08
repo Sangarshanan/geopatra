b4f4b3aa10b2c0bf478c26a5ddd5362eff97e763
sangarshanan <sangarshanan1998@gmail.com>
2020-02-08 15:58:16 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
